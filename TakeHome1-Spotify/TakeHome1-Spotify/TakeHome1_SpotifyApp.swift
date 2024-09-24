eb75ee4067d03ac59028785c48de38aa0284b180
Tri Nguyen <tringuye@unc.edu>
2024-09-24 17:35:45 -0400

    Night by Keshi

---
+++ TakeHome1-Spotify/TakeHome1-Spotify/TakeHome1_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHome1_SpotifyApp.swift
+//  TakeHome1-Spotify
+//
+//  Created by Tri Nguyen on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TakeHome1_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
